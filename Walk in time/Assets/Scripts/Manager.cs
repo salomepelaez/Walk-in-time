f05c02c80097ebae89cc73e3328c476655a3c69b
Salomé Pelaez <spsalome@gmail.com>
2020-08-04 17:36:41 -0500

    Movement test

---
+++ Walk in time/Assets/Scripts/Manager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public static Manager Instance;
+
+    public bool isMoving;
+
+    public void Awake()
+    {
+        Instance = this;
+    }
+
+    public void start()
+    {
+        isMoving = false;
+    }
+
+    public void StartMoving()
+    {
+        isMoving = true;
+    }
+}
---
+++ Walk in time/Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private float speed;
+
+    Manager manager;
+
+    void Start()
+    {
+        speed = 30f;
+        manager = Manager.Instance;
+    }
+
+    public void Move()
+    {
+        if(manager.isMoving == true)
+        {
+            gameObject.transform.position += transform.forward * speed * Time.deltaTime;
+        }
+    } 
+}
