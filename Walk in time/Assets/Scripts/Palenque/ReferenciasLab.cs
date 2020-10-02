eee96c7b567bac84b5827cb8d3eef9d07833241b
Salomé Pelaez <spsalome@gmail.com>
2020-10-02 11:44:59 -0500

    Laberinto

---
+++ Walk in time/Assets/Scripts/Palenque/ReferenciasLab.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReferenciasLab : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.GetComponent<Laberinto>() != null)
+        {
+            Debug.Log("Los puntos funcionan");
+        }
+    }
+}
