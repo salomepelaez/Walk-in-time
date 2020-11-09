e798ea15919facb08fdb9108d9d6b9839bd64cb7
Salomé Pelaez <spsalome@gmail.com>
2020-11-09 18:13:10 -0500

    Dictionary

--- Walk in time/Assets/Scripts/Manager/GameManager.cs
+++ Walk in time/Assets/Scripts/Manager/GameManager.cs
@@ -31,8 +31,57 @@ public class GameManager : MonoBehaviour
     {
         Instance = this;
     }
-     
-    public int CountMatches(List<GameObject> required, List<GameObject> taken)
+    
+    public void Update()
+    {
+        if(mState == State.RunningAway)
+        {
+            //Debug.Log("miau");
+            
+        }
+
+        //CheckPattern();
+    }
+
+    public bool CompareList(List<GameObject> required, List<GameObject> taken)
+    {
+        if (required == null || taken == null || required.Count != taken.Count)
+        return false;
+
+        if (required.Count == 0)
+            return true;
+
+        Dictionary<GameObject, int> lookUp = new Dictionary<GameObject, int>();
+        
+        for(int i = 0; i < required.Count; i++)
+        {
+            int count = 0;
+            if (!lookUp.TryGetValue(required[i], out count))
+            {
+                lookUp.Add(required[i], 1);
+                continue;
+            }
+            lookUp[required[i]] = count + 1;
+        }
+        for (int i = 0; i < taken.Count; i++)
+        {
+            int count = 0;
+            if (!lookUp.TryGetValue(taken[i], out count))
+            {
+                return false;
+            }
+            
+            count--;
+            if (count <= 0)
+                lookUp.Remove(taken[i]);
+            else
+                lookUp[taken[i]] = count;
+        }
+
+        return lookUp.Count == 0;
+    }
+
+    /*public int CountMatches(List<GameObject> required, List<GameObject> taken)
     {
         int numMatches = 0;
         int idx = 0;
@@ -41,8 +90,8 @@ public class GameManager : MonoBehaviour
         {
             if( required[idx] == taken[idx] )
             {
-                numMatches++;
-                idx++;
+                numMatches = numMatches + 1;
+                idx = idx + 1;
             }
             else
             {
@@ -50,24 +99,20 @@ public class GameManager : MonoBehaviour
             }
         }
 
-        return numMatches;
-    }
+        Debug.Log(numMatches);
 
-    public void Update()
-    {
-        if(mState == State.RunningAway)
-        {
-            Debug.Log("miau");
-            CheckPattern();
-        }
+        return numMatches;
     }
 
     public void CheckPattern()
     {
-        int numMatches = CountMatches(references, pointReference);
-        if( numMatches == references.Count )
+        if(mState == State.RunningAway)
         {
-            Debug.Log("muy bien");
+            int numMatches = CountMatches(references, pointReference);
+            if( numMatches == references.Count )
+            {
+                Debug.Log("muy bien");
+            }
         }
-    }
+    }*/
 }
